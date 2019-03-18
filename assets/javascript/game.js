a9a6b2fe529ad8248128712ee8cef066c877cab7
Connor Lee <45024235+connorleee@users.noreply.github.com>
2019-03-17 22:35:35 -0700

    psuedo code done

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+// Initial variables
+var wins = 0;
+var losses = 0;
+var prevGameResults = "";
+var totalScore = 0;
+var gameOver = false;
+
+// function to generate random target number between 19-120. display to div
+
+// function to generate random gem values 1-12 per game on reset
+
+// on click function to add specific (but random per game) values to total score
+
+// win case: total score equals target number. treigger gameOver boolean and reset
+
+// loss case: total score > target number. trigger gameOver boolean and reset
+
